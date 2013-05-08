0468764a6e3fec1fe2c302eecb07868934054846
k0nsl <k0nsl@k0nsl-work02.(none)>
2013-05-08 15:39:55 +0200

    initial.

--- hook_incapsula.php
+++ hook_incapsula.php
@@ -11,30 +11,35 @@ if (!defined('IN_PHPBB'))
    exit;
 }
 
-/**
- * This hook injects the anonymizer code
- */
-function hook_inject_anonymizer(&$hook)
-{
-   /* should I simply echo the JS stuff here, or..? */
-   
-   echo '
+define('HEADER_NAME','HTTP_INCAP_CLIENT_IP');
 
-<script src="http://cdn.mastrcoder.com/js/anonymize.js" type="text/javascript"></script>
- 
-<script type="text/javascript"><!--
-protected_links = "";
- 
-auto_anonymize();
-//--></script>   
-   
-        ';
+/**
+ * Our one and only hook
+ */
+function hook_incapsula(&$hook)
+{
+try {
+	
+	//stop process if there is no header
+	if (empty($_SERVER[HEADER_NAME])) throw new Exception('No header defined', 1);
+	
+	//validate header value
+	if (function_exists('filter_var')) {
+		$ip = filter_var($_SERVER[HEADER_NAME], FILTER_VALIDATE_IP);
+		if (false === $ip) throw new Exception('The value is not a valid IP address', 2);
+	}
+	else {
+		$ip = trim($_SERVER[HEADER_NAME]);
+		if (false === preg_match('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/', $ip)) throw new Exception('The value is not a valid IP address', 2);
+	}
+	
+	//At this point the initial IP value is exist and validated
+	$_SERVER['REMOTE_ADDR'] = $ip;
+} catch (Exception $e) {}
 }
 
 /**
- * We want it only on normal pages and not administrative ones.
+ * Initialize our hook
  */
-if (!defined('ADMIN_START'))
-{
-   $phpbb_hook->register(array('template', 'display'), 'hook_inject_anonymizer');
-}
+
+$phpbb_hook->register(array('template', 'display'), 'hook_incapsula');
